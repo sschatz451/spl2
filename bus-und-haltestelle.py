f52ced0a20f8e8ec2627f8ee249e20e0afa7fde7
sschatz451 <sebastian@pandh-entertainment.com>
2018-04-09 14:21:21 +0200

    Programm Teil 1 fertig

--- bus-und-haltestelle.py
+++ bus-und-haltestelle.py
@@ -1,1 +1,12 @@
 # bus-und-haltestelle.py
+personen = 0
+anzahl_haltestellen = input("Bitte geben Sie die Anzahl der Haltestellen ein: ")
+x = True
+counter_while = 1
+while(x):
+	if(counter_while == int(anzahl_haltestellen)):
+		x = False
+	eingabe = input("Bitte geben Sie ein wieviele Personen einsteigen: ")
+	personen = personen + int(eingabe)
+	counter_while = counter_while + 1
+	print("Es befinden sich ", personen, " Personen im Bus.")
